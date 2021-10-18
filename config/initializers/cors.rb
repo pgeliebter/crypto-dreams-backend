eb6110e15e7cf66a1c20eb2842fae5474430d411
Pinny Geliebter <pgibralter@gmail.com>
2021-10-18 16:29:46 -0400

    Adds platform for x86_64-linux

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080"
+    origins "localhost:8080", "https://dazzling-montalcini-bec337.netlify.app"
 
     resource "*",
       headers: :any,
