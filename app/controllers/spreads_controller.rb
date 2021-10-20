77da1c1fbe63812151d948be6a9d5c93fe42956a
Pinny Geliebter <pgibralter@gmail.com>
2021-10-20 16:05:11 -0400

    adds a commented out route/action to get all exchange assets from shrimpy

--- app/controllers/spreads_controller.rb
+++ app/controllers/spreads_controller.rb
@@ -1,17 +1,27 @@
 class SpreadsController < ApplicationController
   def shrimpy_all
-    puts params
     query_params = { exchange: "all", limit: "1", baseSymbol: params[:base], quoteSymbol: params[:quote] }
     puts query_params
     response = Faraday.get("https://dev-api.shrimpy.io/v1/orderbooks", query_params, "DEV-SHRIMPY-API-KEY": Rails.application.credentials.shrimpy[:api_key])
     if response.success?
       # I want to map through my response and return only exchanges that have order books
-      boo = JSON.parse(response.body)
-      boo[0]["orderBooks"].reject! { |book| book["orderBook"].nil? }
-      render json: boo
+      formatted_response = JSON.parse(response.body)
+      formatted_response[0]["orderBooks"].reject! { |book| book["orderBook"].blank? }
+      render json: formatted_response
       puts response
     else
       render json: { errors: JSON.parse(response.body) }, status: response.status
     end
   end
+
+  # end point to get all supported currency for all exchanges
+  # def get_exchange_assets
+  #   response = Faraday.get("https://dev-api.shrimpy.io/v1/exchanges/#{params[:exchange]}/assets", {}, "DEV-SHRIMPY-API-KEY": Rails.application.credentials.shrimpy[:api_key])
+  #   if response.success?
+  #     formatted_response = JSON.parse(response.body)
+  #     render json: formatted_response
+  #   else
+  #     render json: { errors: JSON.parse(response.body) }, status: response.status
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "spreads" => "spreads#shrimpy_all"
+
+  # end point to get all supported currency for all exchanges
+  # get "exchanges/assets" => "spreads#get_exchange_assets"
 end
