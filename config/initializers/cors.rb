3b04ceefc0103e7ff0e2b258058df1c69106bfe3
Pinny Geliebter <pgibralter@gmail.com>
2021-10-19 21:27:14 -0400

    changes call to crypto-dreams

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "https://dazzling-montalcini-bec337.netlify.app"
+    origins "localhost:8080", "https://crypto-dreams.netlify.app"
 
     resource "*",
       headers: :any,
