eefbc0f0c307c545dc192e54f9a0d0cd1b2f5e8a
Pinny Geliebter <pgibralter@gmail.com>
2021-10-21 13:02:51 -0400

    updates cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "https://crypto-dreams.netlify.app"
+    origins "*", "https://crypto-dreams.netlify.app"
 
     resource "*",
       headers: :any,
