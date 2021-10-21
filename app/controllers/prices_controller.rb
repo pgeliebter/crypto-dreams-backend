aa865c3348d84adf340a67f4ad5ca986f0cec816
Pinny Geliebter <pgibralter@gmail.com>
2021-10-21 11:53:09 -0400

    adds comment and remove puts

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,7 +1,7 @@
 class PricesController < ApplicationController
   def market_chart
+    # hard coded currency for now
     response = Faraday.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=hourly")
-    puts response
     if response.success?
       render json: JSON.parse(response.body), status: response.status
     else
@@ -10,8 +10,8 @@ class PricesController < ApplicationController
   end
 
   def current_price
+    # hard coded currency for now
     response = Faraday.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true")
-    puts response
     if response.success?
       render json: JSON.parse(response.body), status: response.status
     else
