49900147035696af9eee3e0975cb5029aa9d0297
Pinny Geliebter <pgibralter@gmail.com>
2021-10-26 10:48:42 -0400

    adds comments and removes unused functions

--- app/controllers/spreads_controller.rb
+++ app/controllers/spreads_controller.rb
@@ -1,16 +1,21 @@
 class SpreadsController < ApplicationController
   def shrimpy_all
+    # setting up my query params for the call
     query_params = { exchange: "all", limit: "1", baseSymbol: params[:base], quoteSymbol: params[:quote] }
+    # check if the user has correct master key to unlock api key otherwise not necessary
     if Rails.application.credentials.shrimpy
       key = Rails.application.credentials.shrimpy[:api_key]
     end
+    # call the shrimpy api with the query params and the key
     response = Faraday.get("https://dev-api.shrimpy.io/v1/orderbooks", query_params, "DEV-SHRIMPY-API-KEY": key)
-    if response.success?
-      # I want to map through my response and return only exchanges that have order books
+
+    if response.success? # happy path
+      # looping through response and return only exchanges that have order books as blanks are included in response
       formatted_response = JSON.parse(response.body)
       formatted_response[0]["orderBooks"].reject! { |book| book["orderBook"].blank? }
       render json: formatted_response
-    else
+    else # sad path
+      # check if rate limit has been exceeded and send prompt to add correct master key to unlock api key
       if response.status == 429
         render json: { message: "Please make sure the master key was added correctly.", errors: JSON.parse(response.body) }, status: response.status
       else
@@ -18,15 +23,4 @@ class SpreadsController < ApplicationController
       end
     end
   end
-
-  # end point to get all supported currency for all exchanges
-  # def get_exchange_assets
-  #   response = Faraday.get("https://dev-api.shrimpy.io/v1/exchanges/#{params[:exchange]}/assets", {}, "DEV-SHRIMPY-API-KEY": Rails.application.credentials.shrimpy[:api_key])
-  #   if response.success?
-  #     formatted_response = JSON.parse(response.body)
-  #     render json: formatted_response
-  #   else
-  #     render json: { errors: JSON.parse(response.body) }, status: response.status
-  #   end
-  # end
 end
